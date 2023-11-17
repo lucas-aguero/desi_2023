94bd481536e8fac22b2bd396092a158c78f49cfd
FlaminiaCastano <88913777+FlaminiaCastano@users.noreply.github.com>
2023-11-17 19:30:08 -0300

    Creación de Entidad de Datos Impositivos

---
+++ src/main/java/tuti/desi/entidades/DatosImpositivos.java
@@ -0,0 +1,92 @@
+package tuti.desi.entidades;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class DatosImpositivos {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotNull
+	@DecimalMin("0.00")
+	@DecimalMax("100.0")
+	private Double iva;
+	
+	@NotNull
+	@DecimalMin("0.00")
+	private Double tasaAeroportuariaNacional;
+	
+	@NotNull
+	@DecimalMin("0.00")
+	private Double tasaAeroportuariaInternacional;
+	
+	@NotNull
+	@DecimalMin("0.00")
+	private Double cotizacionDolar;
+	
+	 @NotNull
+	 @Column(name = "fecha_modificacion")
+	 private LocalDateTime fechaModificacion;
+	
+	// Getter y setters
+
+	public Double getIva() {
+		return iva;
+	}
+
+	public void setIva(Double iva) {
+		this.iva = iva;
+	}
+
+	public Double getTasaAeroportuariaNacional() {
+		return tasaAeroportuariaNacional;
+	}
+
+	public void setTasaAeroportuariaNacional(Double tasaAeroportuariaNacional) {
+		this.tasaAeroportuariaNacional = tasaAeroportuariaNacional;
+	}
+
+	public Double getTasaAeroportuariaInternacional() {
+		return tasaAeroportuariaInternacional;
+	}
+
+	public void setTasaAeroportuariaInternacional(Double tasaAeroportuariaInternacional) {
+		this.tasaAeroportuariaInternacional = tasaAeroportuariaInternacional;
+	}
+
+	public Double getCotizacionDolar() {
+		return cotizacionDolar;
+	}
+
+	public void setCotizacionDolar(Double cotizacionDolar) {
+		this.cotizacionDolar = cotizacionDolar;
+	}
+
+	public LocalDateTime getFechaModificacion() {
+		return fechaModificacion;
+	}
+
+	public void setFechaModificacion(LocalDateTime fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}	
+	
+}
