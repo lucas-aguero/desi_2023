33c8619d49f3404b4aef7cf3980061a042d887db
brunovt074 <brunodev.pdl@gmail.com>
2023-11-21 00:46:57 -0300

    Creadas entidades Aeropuerto, Vuelo, ENUM TipoVuelo

---
+++ src/main/java/tuti/desi/entidades/Aeropuerto.java
@@ -0,0 +1,36 @@
+package tuti.desi.entidades;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="aeropuertos")
+public class Aeropuerto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "icao", unique = true)
+    private String icao;
+    private String iata;
+    @NotNull
+    private String name;
+    @NotNull
+    private String city;
+    @NotNull
+    private String state;
+    @NotNull
+    private String country;
+    private long elevation;
+    private double lat;
+    private double lon;
+    private String tz;
+    @OneToMany(mappedBy = "origen")
+    private List<Vuelo> vuelosOrigen;
+    @OneToMany(mappedBy = "destino")
+    private List<Vuelo> vuelosDestino;
+}
---
+++ src/main/java/tuti/desi/entidades/Vuelo.java
@@ -0,0 +1,68 @@
+package tuti.desi.entidades;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import tuti.desi.entidades.enums.TipoVuelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@Table(name = "vuelos")
+public class Vuelo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nro_vuelo")
+    private Long nroVuelo;
+    @Column(name = "fecha_partida")
+    @DateTimeFormat
+    @NotNull
+    private LocalDate fechaPartida;
+    @Column(name = "hora_partida")
+    @DateTimeFormat
+    @NotNull
+    private LocalTime horaPartida;
+    private String avion;
+    @Column(name = "nro_filas")
+    private int nroFilas;
+    @Column(name = "asiento_por_fila")
+    private int asientosPorFila;
+    @Column(name = "nro_asientos")
+    private int nroAsientos;
+    //Origen y destino
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="origen_id")
+    private Aeropuerto origen;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="destino_id")
+    private Aeropuerto destino;
+    @Column(name="tipo_vuelo")
+    private TipoVuelo tipoVuelo;
+    @NotNull
+    private BigDecimal precio;
+
+    public void setNroAsientos(int nroFilas, int asientosPorFila) {
+        this.nroAsientos = nroFilas * asientosPorFila;
+    }
+
+    @Autowired //fuerzo a spring que utilice este metodo durante la IOC
+    public void setTipoVuelo() {
+        String argentina = "AR";
+
+        if(argentina.equalsIgnoreCase(this.destino.getCountry())) {
+            this.tipoVuelo = TipoVuelo.NACIONAL;
+        }
+        else {
+            this.tipoVuelo = TipoVuelo.INTERNACIONAL;
+        }
+    }
+
+}
